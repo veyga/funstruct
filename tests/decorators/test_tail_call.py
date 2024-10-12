4783b170ffac6a2b35085087495fe57f9da3f990
Andrew Stefanich <andrew.stefanich@buoyhealth.com>
2024-10-12 17:34:20 -0400

    tco dec not working; combo working

--- tests/decorators/test_tail_call.py
+++ tests/decorators/test_tail_call.py
@@ -5,10 +5,12 @@ from funstruct.decorators import TailCaller, tailcall
 # pytest affects the default of 1000
 RECURSION_LIMIT = 700
 
+
 def n_called(remaining: int):
-  if remaining == 0:
-    return 0
-  return 1 + n_called(remaining - 1)
+    if remaining == 0:
+        return 0
+    return 1 + n_called(remaining - 1)
+
 
 # @pytest.mark.skip()
 # def test_no_tailrec_under_limit():
@@ -26,16 +28,37 @@ def n_called(remaining: int):
 # def test_tailrec_recurses_beyond():
 #   limit = RECURSION_LIMIT * 2
 #   count = n_called_tailrec(limit)
-#   assert count == limit 
+#   assert count == limit
 
-@TailCaller
-def n_called_tailrec(remaining: int, acc:int = 0):
-  if remaining == 0:
-    return acc
-  return tailcall(n_called_tailrec)(remaining -1, acc + 1)
 
-def test_tailrec_recurses_beyond_decorated():
-  limit = RECURSION_LIMIT * 2
-  count = n_called_tailrec(limit)
-  assert count == limit 
-  print("count = ", count)
+def test_tailrec_recurses_beyond_decorated_1():
+    @TailCaller
+    def recurse(remaining: int, acc: int = 0):
+        if remaining == 0:
+            return acc
+        return tailcall(recurse)(remaining - 1, acc + 1)
+
+    limit = RECURSION_LIMIT * 2
+    count = recurse(limit)
+    assert count == limit
+
+
+def test_tailrec_recurses_beyond_decorated_2():
+    def recurse(n: int):
+        @TailCaller
+        def inner(remaining: int, acc: int):
+            if remaining == 0:
+                return acc
+            return tailcall(inner)(remaining - 1, acc + 1)
+
+        return inner(n, 0)
+
+    limit = RECURSION_LIMIT * 2
+    count = recurse(limit)
+    assert count == limit
+
+
+# def test_tailrec_recurses_beyond_decorated_yo():
+#   limit = RECURSION_LIMIT * 2
+#   count = recurse_n_times(limit)
+#   assert count == limit
